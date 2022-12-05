b6b52eef1f364875fc7565f2dec650ba08a58a50
Lucas LOUIS <lucas.louis@epitech.eu>
2022-12-05 16:26:25 +0100

    feat(assets): add logos

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,7 +15,7 @@ const App = ({ Component, pageProps }: AppProps): JSX.Element => (
       />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta charSet="UTF-8" />
-      <link rel="icon" href="/assets/edgar.care-logo.svg" />
+      <link rel="icon" href="/assets/edgar.care-logo-tabs.svg" />
     </Head>
     <ChakraProvider theme={theme} resetCSS>
       <Component {...pageProps} />
