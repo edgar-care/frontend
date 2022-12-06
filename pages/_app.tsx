664f4159a52b3c93665f7d283547e5c20569b097
Lucas LOUIS <lucas.louis@epitech.eu>
2022-12-06 16:42:16 +0100

    feat(auth): add auth context & utils function + auth management with protected/unprotected pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,56 @@
 import type { AppProps } from 'next/app';
-import { ChakraProvider } from '@chakra-ui/react';
+import { Center, ChakraProvider, useToast } from '@chakra-ui/react';
 import Head from 'next/head';
 
 import theme from 'theme';
 import 'theme/index.css';
+import { useEffect, useState } from 'react';
+import { ChaoticOrbit } from '@uiball/loaders';
+import AuthContext from '../src/contexts/auth';
+import Auth from '../src/libs/auth';
 
-const App = ({ Component, pageProps }: AppProps): JSX.Element => (
-	<>
-		<Head>
-			<title>edgar.care</title>
-			<meta name="description" content="" />
-			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-			<meta charSet="UTF-8" />
-			<link rel="icon" href="/assets/edgar.care-logo-tabs.svg" />
-		</Head>
-		<ChakraProvider theme={theme} resetCSS>
-			<Component {...pageProps} />
-		</ChakraProvider>
-	</>
-);
+const App = ({ Component, pageProps }: AppProps): JSX.Element => {
+	const [auth, setAuth] = useState<Auth | undefined>(undefined);
+
+	const toast = useToast({ duration: 2000, isClosable: true });
+
+	useEffect(() => {
+		if (!auth) {
+			try {
+				setAuth(new Auth());
+			} catch (e) {
+				toast({
+					title: 'Erreur interne au serveur',
+					status: 'error',
+				});
+			}
+		}
+	}, []);
+
+	if (!auth) {
+		return (
+			<Center>
+				<ChaoticOrbit color="pink.500" />
+			</Center>
+		);
+	}
+
+	return (
+		<>
+			<Head>
+				<title>edgar.care</title>
+				<meta name="description" content="" />
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+				<meta charSet="UTF-8" />
+				<link rel="icon" href="/assets/edgar.care-logo-tabs.svg" />
+			</Head>
+			<ChakraProvider theme={theme} resetCSS>
+				<AuthContext.Provider value={auth}>
+					<Component {...pageProps} />
+				</AuthContext.Provider>
+			</ChakraProvider>
+		</>
+	);
+};
 
 export default App;
---
+++ src/components/pages/ProtectedPage.tsx
@@ -0,0 +1,41 @@
+import { Center, Img, VStack } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import { MrMiyagi } from '@uiball/loaders';
+
+import useToggle from 'hooks/useToggle';
+
+import { useAuthContext } from 'contexts/auth';
+
+import colors from 'theme/foundations/colors';
+
+const ProtectedPage = ({ children }: { children: JSX.Element }): JSX.Element => {
+	const auth = useAuthContext();
+	const router = useRouter();
+
+	const { toggle: isAuthenticated, toggleHandler: authenticateHandler } = useToggle();
+
+	useEffect(() => {
+		auth.checkToken().then((res) => {
+			if (res.status !== 'success') void router.push('/login');
+			else authenticateHandler();
+		});
+	}, []);
+
+	return (
+		<>
+			{isAuthenticated ? (
+				<VStack p="128px">{children}</VStack>
+			) : (
+				<Center h="100vh">
+					<VStack spacing="128px">
+						<Img src="/assets/edgar.care-logo.svg" w="350px" />
+						<MrMiyagi size={64} color={colors.blue[900]} />
+					</VStack>
+				</Center>
+			)}
+		</>
+	);
+};
+
+export default ProtectedPage;
