8ef343e752f2249a76288fd1ff81f7cc91766707
Lucas LOUIS <lucas.louis@epitech.eu>
2022-12-08 16:03:12 +0100

    feat(pages): remove the blue background on all pages

--- src/components/pages/BasicPage.tsx
+++ src/components/pages/BasicPage.tsx
@@ -1,9 +1,5 @@
 import { VStack } from '@chakra-ui/react';
 
-const BasicPage = ({ children }: { children: JSX.Element }): JSX.Element => (
-	<VStack bg="blue.50" minH="100vh">
-		{children}
-	</VStack>
-);
+const BasicPage = ({ children }: { children: JSX.Element }): JSX.Element => <VStack minH="100vh">{children}</VStack>;
 
 export default BasicPage;
