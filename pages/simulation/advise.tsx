c197cadff1a1c1f0938f4fa5bf934706146bfd6a
Lucas LOUIS <lucas.louis@epitech.eu>
2022-12-12 17:26:16 +0100

    fix(redirection): fix for the PR

--- pages/simulation/advise.tsx
+++ pages/simulation/advise.tsx
@@ -1,29 +1,26 @@
 import { Text, VStack, Stack, HStack, Button, Link } from '@chakra-ui/react';
 
-import ColorText from 'components/GradientText';
 import SimulationPage from 'components/pages/simulation/SimulationPage';
+import GradientText from 'components/GradientText';
 
 const Advise = (): JSX.Element => (
 	<SimulationPage>
 		<VStack spacing="128px">
 			<Stack spacing="64px" maxW="650px">
 				<Text size="2xl">
-					Vous receverez une <ColorText textValue="notification" /> lorsque le medecin aura répondu
+					Vous receverez une <GradientText textValue="notification" /> lorsque le médecin aura répondu
 				</Text>
 				<Text size="2xl">
 					En attendant sa réponse, je vous conseille de faire une sieste dans un endroit{' '}
-					<ColorText textValue="calme" /> et <ColorText textValue="peu lumineux" />
+					<GradientText textValue="calme" /> et <GradientText textValue="peu lumineux" />
 				</Text>
 			</Stack>
 			<HStack spacing="64px">
 				<Link href="/app/patient">
 					<Button variant="primary" size="lg">
-						Acceder a mon compte
+						Accéder à mon compte
 					</Button>
 				</Link>
-				<Button onClick={window.close} variant="secondary" size="lg">
-					Fermer la page
-				</Button>
 			</HStack>
 		</VStack>
 	</SimulationPage>
