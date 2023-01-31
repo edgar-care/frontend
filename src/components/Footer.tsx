c36ad7e27119ba39433b4a7d91d18ad374a867d0
Lucas LOUIS <lucas.louis@epitech.eu>
2023-01-31 09:57:05 +0100

    feat(simulation): add redirection to home page to cancel simulation

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,12 @@
-import { Box, Divider, Image, VStack } from '@chakra-ui/react';
+import { Box, Divider, Image, Link, VStack } from '@chakra-ui/react';
 
 const Footer = (): JSX.Element => (
 	<Box as="footer" w="100vw" position="fixed" zIndex="10" bottom="0" bg="white">
 		<Divider />
 		<VStack py="16px">
-			<Image src="/assets/edgar.care-logo.svg" alt="edgar.care-footer" w="150px" h="auto" />
+			<Link href="/">
+				<Image src="/assets/edgar.care-logo.svg" alt="edgar.care-footer" w="150px" h="auto" />
+			</Link>
 		</VStack>
 	</Box>
 );
