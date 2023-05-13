ffb19b21f63597d21325fcdcf0e8880c409201d5
Lucas LOUIS <lucas.louis@epitech.eu>
2023-05-13 12:05:18 +0200

    feat(theme): add the main theme

---
+++ landing-pages/src/theme/components/link.ts
@@ -0,0 +1,16 @@
+const Link = {
+	baseStyle: {
+		_focus: {
+			boxShadow: 'none',
+		},
+		_focusVisible: {
+			outline: 'none',
+		},
+		_hover: {
+			textDecoration: 'none',
+		},
+	},
+	defaultProps: {},
+};
+
+export default Link;
---
+++ landing-pages/src/theme/components/text.ts
@@ -0,0 +1,78 @@
+const Text = {
+	baseStyle: {
+		color: 'black',
+		fontFamily: 'Poppins',
+	},
+	sizes: {
+		sm: {
+			fontSize: '12px',
+			fontWeight: 500,
+		},
+		boldSm: {
+			fontSize: '12px',
+			fontWeight: 700,
+		},
+		md: {
+			fontSize: '14px',
+			fontWeight: 400,
+		},
+		boldMd: {
+			fontSize: '14px',
+			fontWeight: 700,
+		},
+		lg: {
+			fontSize: '16px',
+			fontWeight: 500,
+		},
+		boldLg: {
+			fontSize: '16px',
+			fontWeight: 700,
+		},
+		xl: {
+			fontSize: '20px',
+			fontWeight: 500,
+		},
+		boldXl: {
+			fontSize: '20px',
+			fontWeight: 700,
+		},
+		'2xl': {
+			fontSize: '24px',
+			fontWeight: 500,
+		},
+		bold2xl: {
+			fontSize: '24px',
+			fontWeight: 700,
+		},
+		'3xl': {
+			fontSize: '32px',
+			fontWeight: 700,
+		},
+		'4xl': {
+			fontSize: '36px',
+			fontWeight: 700,
+		},
+		'5xl': {
+			fontSize: '42px',
+			fontWeight: 700,
+		},
+		'6xl': {
+			fontSize: '48px',
+			fontWeight: 700,
+		},
+		'7xl': {
+			fontSize: '64px',
+			fontWeight: 700,
+		},
+		'8xl': {
+			fontSize: '72px',
+			fontWeight: 700,
+		},
+	},
+	variants: {},
+	defaultProps: {
+		size: 'md',
+	},
+};
+
+export default Text;
