0727ad03522dd14bb1db1feb715f8d6fca90ca83
Lucas LOUIS <lucas.louis@epitech.eu>
2023-05-22 21:56:39 +0200

    feat(airtable): add connection to the airtable for contact & newsletter forms

--- landing-pages/src/utils/subscribeToNewsletter.ts
+++ landing-pages/src/utils/subscribeToNewsletter.ts
@@ -1,10 +1,30 @@
+import { AIRTABLE_KEY } from 'config/constants';
+
 import { type MessageResponse } from 'types/MessageResponse';
 
 const subscribeToNewsletter = async (email: string): Promise<MessageResponse> => {
 	try {
 		if (!/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(email))
 			return { title: 'Adresse mail invalide', status: 'error' };
-		// CALL BACK
+
+		const res = await fetch('https://api.airtable.com/v0/apppNB1HHznqlQND3/tblHPux1OBUFVId1S', {
+			method: 'POST',
+			headers: {
+				Authorization: `Bearer ${AIRTABLE_KEY}`,
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				records: [
+					{
+						fields: {
+							Email: email,
+							Date: new Date().toISOString(),
+						},
+					},
+				],
+			}),
+		});
+		if (res.status !== 200) return { title: 'Une erreur est survenue, merci de réessayer', status: 'error' };
 		return { title: 'Abonnement à la newsletter réussi', status: 'success' };
 	} catch (error) {
 		console.error(error);
