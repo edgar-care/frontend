ca1b34235d6daaafe6b09878bdb22f899708aacb
Jules <jules.magyari@epitech.eu>
2023-12-08 10:21:09 +0100

    fix(eslint/services): little modification about eslint and apiService

--- edgar-app/src/services/apiService.ts
+++ edgar-app/src/services/apiService.ts
@@ -21,7 +21,7 @@ export const isFetchBaseQueryErrorType = (err: FetchBaseQueryError | SerializedE
 	'status' in err;
 
 export const backendApi = createApi({
-	tagTypes: ['Example', 'patientAppointments', 'doctorAppointments', 'patientMedicalFolder'],
+	tagTypes: ['Example', 'patientAppointments', 'doctorAppointments', 'patientMedicalFolder', 'patientDocuments'],
 	reducerPath: 'backendApi',
 	baseQuery: apiBase,
 	endpoints: () => ({}),
