7bc009738aa66c2d23eeb558982c6ce2c990a0e3
Jules <jules.magyari@epitech.eu>
2023-12-16 15:41:07 +0100

    fix(dashboard-document): update some spacing, delete useless icon and fix fill property

--- edgar-app/src/app/dashboard/documents/content.tsx
+++ edgar-app/src/app/dashboard/documents/content.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState } from 'react';
-import { VStack, Button, HStack, Input, InputGroup, InputRightElement, useDisclosure } from '@chakra-ui/react';
+import { VStack, Button, HStack, Input, InputGroup, InputRightElement, useDisclosure, Icon } from '@chakra-ui/react';
 
 import DocumentCard from 'components/dashboardPages/documents/DocumentCard';
 import DashboardPageBanner from 'components/dashboardPages/DashboardPageBanner';
@@ -12,8 +12,6 @@ import AddDocumentModal from 'components/dashboardPages/documents/modal/AddDocum
 import { DocumentType } from 'types/dashboard/documents/DocumentType';
 
 import SearchIcon from 'assets/icons/SearchIcon';
-
-// import DocumentFirstFilter from 'components/dashboardPages/documents/DocumentFirstFilter';
 
 const DocumentsPageContent = (): JSX.Element => {
 	// const { data: documentsData } = useGetDocumentsQuery();
@@ -44,65 +42,37 @@ const DocumentsPageContent = (): JSX.Element => {
 	const { isOpen: isOpenAddModal, onOpen: onOpenAddModal, onClose: onCloseAddModal } = useDisclosure();
 	const [searchTerm, setSearchTerm] = useState('');
 	void searchTerm;
-	// const [sortOrder, setSortOrder] = useState('newest');
-
-	// const handleFilterChange = (value: string) => {
-	// 	setSortOrder(value);
-	// 	const sortedDocuments = [...documentsData].sort((a, b) => {
-	// 	  switch (value) {
-	// 		case 'asc':
-	// 		  return a.name.localeCompare(b.name);
-	// 		case 'desc':
-	// 		  return b.name.localeCompare(a.name);
-	// 		case 'newest':
-	// 		  return new Date(b.date).getTime() - new Date(a.date).getTime();
-	// 		case 'oldest':
-	// 		  return new Date(a.date).getTime() - new Date(b.date).getTime();
-	// 		default:
-	// 		  return 0;
-	// 	  }
-	// 	});
-	// 	setDocumentsData(sortedDocuments);
-	//   };
 
 	return (
-		<VStack w="100%" spacing="16px">
+		<VStack w="100%" spacing="32px">
 			<DashboardPageBanner
 				title="Mes documents"
 				subTitle="Retrouvez toutes vos documents personnels et médicaux."
 			/>
-			<VStack w="100%" spacing="24px">
-				<HStack w="100%" spacing="16px">
-					<Button size="customMd" variant="primary" whiteSpace="nowrap" onClick={onOpenAddModal}>
-						Ajouter un document
-					</Button>
-					<InputGroup w="100%">
-						<Input
-							placeholder="Rechercher par nom du document ou nom du médecin"
-							onChange={(e) => setSearchTerm(e.target.value)}
-						/>
-						<InputRightElement>
-							<SearchIcon />
-							{/* Transformer en icon as SearchIcon */}
-						</InputRightElement>
-					</InputGroup>
-				</HStack>
-				<HStack w="100%">
-					{/* <DocumentFirstFilter onFilterChange={handleFilterChange} sortOrder={sortOrder} /> */}
-				</HStack>
-			</VStack>
-			<VStack spacing="8px" w="100%" align="start">
-				{documentsData
-					// .filter(
-					// 	(document) =>
-					// 		document.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
-					// 		document.author.toLowerCase().includes(searchTerm.toLowerCase()),
-					// )
-					.map((document) => (
+			<VStack w="100%" spacing="16px">
+				<VStack w="100%" spacing="24px">
+					<HStack w="100%" spacing="16px">
+						<Button size="customMd" variant="primary" whiteSpace="nowrap" onClick={onOpenAddModal}>
+							Ajouter un document
+						</Button>
+						<InputGroup w="100%">
+							<Input
+								placeholder="Rechercher par nom du document ou nom du médecin"
+								onChange={(e) => setSearchTerm(e.target.value)}
+							/>
+							<InputRightElement>
+								<Icon as={SearchIcon} w="16px" h="16px" />
+							</InputRightElement>
+						</InputGroup>
+					</HStack>
+				</VStack>
+				<VStack spacing="8px" w="100%" align="start">
+					{documentsData.map((document) => (
 						<DocumentCard key={document._id} document={document} />
 					))}
+				</VStack>
+				<AddDocumentModal isOpen={isOpenAddModal} onClose={onCloseAddModal} />
 			</VStack>
-			<AddDocumentModal isOpen={isOpenAddModal} onClose={onCloseAddModal} />
 		</VStack>
 	);
 };
--- edgar-app/src/components/dashboardPages/documents/DocumentFirstFilter.tsx
+++ edgar-app/src/components/dashboardPages/documents/DocumentFirstFilter.tsx
@@ -6,49 +6,41 @@ import DownArrowIcon from 'assets/icons/Arrow/DownArrowIcon';
 import CalendarIcon from 'assets/icons/CalendarIcon';
 
 interface DocumentFirstFilterProps {
-  onFilterChange: (value: string) => void;
-  sortOrder: string;
+	onFilterChange: (value: string) => void;
+	sortOrder: string;
 }
 
 const DocumentFirstFilter: React.FC<DocumentFirstFilterProps> = ({ onFilterChange, sortOrder }) => {
-  const getIcon = (menuItemValue: string) => {
-    switch (menuItemValue) {
-      case 'asc':
-        return <Icon as={UpArrowIcon} w="12px" h="12px" />;
-      case 'desc':
-        return <Icon as={DownArrowIcon} w="12px" h="12px" />;
-      case 'newest':
-      case 'oldest':
-        return <Icon as={CalendarIcon} />;
-      default:
-        return null;
-    }
-  };
+	const getIcon = (menuItemValue: string) => {
+		switch (menuItemValue) {
+			case 'asc':
+				return <Icon as={UpArrowIcon} w="12px" h="12px" />;
+			case 'desc':
+				return <Icon as={DownArrowIcon} w="12px" h="12px" />;
+			case 'newest':
+			case 'oldest':
+				return <Icon as={CalendarIcon} />;
+			default:
+				return null;
+		}
+	};
 
-  return (
-    <HStack spacing="8px">
-      <Menu defaultIsOpen>
-        <MenuButton as={HStack} bg="white" p="4px 12px" border="1px" borderColor="blue.700" borderRadius="32">
-          <span>Options de tri</span>
-          {getIcon(sortOrder)}
-        </MenuButton>
-        <MenuList>
-          <MenuItem onClick={() => onFilterChange('newest')}>
-            {getIcon('newest')} Plus récent
-          </MenuItem>
-          <MenuItem onClick={() => onFilterChange('oldest')}>
-            {getIcon('oldest')} Plus ancien
-          </MenuItem>
-          <MenuItem onClick={() => onFilterChange('asc')}>
-            {getIcon('asc')} Tri alphabétique
-          </MenuItem>
-          <MenuItem onClick={() => onFilterChange('desc')}>
-            {getIcon('desc')} Tri antialphabétique
-          </MenuItem>
-        </MenuList>
-      </Menu>
-    </HStack>
-  );
+	return (
+		<HStack spacing="8px">
+			<Menu defaultIsOpen>
+				<MenuButton as={HStack} bg="white" p="4px 12px" border="1px" borderColor="blue.700" borderRadius="32">
+					<span>Options de tri</span>
+					{getIcon(sortOrder)}
+				</MenuButton>
+				<MenuList>
+					<MenuItem onClick={() => onFilterChange('newest')}>{getIcon('newest')} Plus récent</MenuItem>
+					<MenuItem onClick={() => onFilterChange('oldest')}>{getIcon('oldest')} Plus ancien</MenuItem>
+					<MenuItem onClick={() => onFilterChange('asc')}>{getIcon('asc')} Tri alphabétique</MenuItem>
+					<MenuItem onClick={() => onFilterChange('desc')}>{getIcon('desc')} Tri antialphabétique</MenuItem>
+				</MenuList>
+			</Menu>
+		</HStack>
+	);
 };
 
 export default DocumentFirstFilter;
