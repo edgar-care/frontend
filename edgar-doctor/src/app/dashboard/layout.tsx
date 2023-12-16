984dce2a8bc26798a692b5c95ee86dec5e6edcc4
Lucas LOUIS <lucas.louis@epitech.eu>
2023-12-17 00:10:48 +0100

    feat(dashboard): update default padding for the layout of the dashboard

--- edgar-doctor/src/app/dashboard/layout.tsx
+++ edgar-doctor/src/app/dashboard/layout.tsx
@@ -41,7 +41,7 @@ const DashboardLayout = ({ children }: { children: JSX.Element }): JSX.Element =
 							w="100%"
 							h={isDrawer ? '100%' : 'calc(100vh - 36px)'}
 							overflowY="scroll"
-							p="32px"
+							p={{ base: '16px', sm: '32px' }}
 							sx={{
 								'::-webkit-scrollbar': {
 									width: '6px',
