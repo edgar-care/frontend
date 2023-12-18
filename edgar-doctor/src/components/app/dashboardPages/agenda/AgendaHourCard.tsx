5ea454700563e25ab9444f63a2eea75a98ecacec
Lucas LOUIS <lucas.louis@epitech.eu>
2023-12-18 15:21:09 +0100

    feat(agenda): add id for tests

--- edgar-doctor/src/components/app/dashboardPages/agenda/AgendaHourCard.tsx
+++ edgar-doctor/src/components/app/dashboardPages/agenda/AgendaHourCard.tsx
@@ -2,7 +2,9 @@ import { Text, VStack } from '@chakra-ui/react';
 
 const AgendaHourCard = ({ hour }: { hour: number }): JSX.Element => (
 	<VStack w="42px" minH="100px" p="8px 4px" borderTop="2px solid" borderColor="blue.200">
-		<Text size="sm">{hour < 10 ? `0${hour}:00` : `${hour}:00`}</Text>
+		<Text size="sm" id="edgar-dashboardAgendaPage-calendarHour-text">
+			{hour < 10 ? `0${hour}:00` : `${hour}:00`}
+		</Text>
 	</VStack>
 );
 
