3d4396c13dd71ac81228a29324baaa31f3c1e9d2
Lucas LOUIS <lucas.louis@epitech.eu>
2023-12-23 20:05:19 +0100

    feat(.env): add new env variable

--- edgar-app/src/config/constants.ts
+++ edgar-app/src/config/constants.ts
@@ -1,6 +1,10 @@
 if (!process.env.NEXT_PUBLIC_API_URL) throw new Error('NEXT_PUBLIC_API_URL env variable must be set');
 if (!process.env.NEXT_PUBLIC_LANDING_URL) throw new Error('NEXT_PUBLIC_LANDING_URL env variable must be set');
 
+if (!process.env.NEXT_PUBLIC_VAPIDPUBLICKEY) throw new Error('NEXT_PUBLIC_VAPIDPUBLICKEY env variable must be set');
+
 // eslint-disable-next-line import/prefer-default-export
 export const API_URL = process.env.NEXT_PUBLIC_API_URL;
 export const LANDING_URL = process.env.NEXT_PUBLIC_LANDING_URL;
+
+export const VAPIDPUBLICKEY = process.env.NEXT_PUBLIC_VAPIDPUBLICKEY;
