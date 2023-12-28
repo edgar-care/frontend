1955ab63d0ad51d3020d4a2ff9b15be9edab7b5f
Lucas LOUIS <lucas.louis@epitech.eu>
2023-12-28 19:15:38 +0100

    feat(agenda): add service to backend

---
+++ edgar-doctor/src/store/types/slots.type.ts
@@ -0,0 +1,12 @@
+export interface SlotsStoreType {
+	id: string;
+	doctor_id: string;
+	id_patient: string;
+	start_date: number;
+	end_date: number;
+}
+
+export interface OpenSlotDTO {
+	start_date: number;
+	end_date: number;
+}
