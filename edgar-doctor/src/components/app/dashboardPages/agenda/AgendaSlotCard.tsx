a6a56fe95658905dfdae189e734a3e0fc9ad8554
Lucas LOUIS <lucas.louis@epitech.eu>
2024-01-04 18:25:09 +0100

    feat(agenda): add closing slot feature

--- edgar-doctor/src/components/app/dashboardPages/agenda/AgendaSlotCard.tsx
+++ edgar-doctor/src/components/app/dashboardPages/agenda/AgendaSlotCard.tsx
@@ -1,10 +1,11 @@
 import { Box, HStack, Icon, Text, useDisclosure, useToast, VStack } from '@chakra-ui/react';
 
-import { useOpenSlotMutation } from 'services/request/slots';
+import { useCloseSlotMutation, useOpenSlotMutation } from 'services/request/slots';
 
 import { type AgendaSlotStatusType } from 'types/app/dashboard/agenda/AgendaSlotType';
 
 import CircleCheckIcon from 'assets/icons/Circle/CircleCheckIcon';
+import CircleCrossIcon from 'assets/icons/Circle/CIrcleCrossIcon';
 
 const AgendaClosedSlotCard = ({ startDate, endDate }: { startDate: number; endDate: number }): JSX.Element => {
 	const [triggerOpenSlot] = useOpenSlotMutation();
@@ -46,20 +47,48 @@ const AgendaClosedSlotCard = ({ startDate, endDate }: { startDate: number; endDa
 		</VStack>
 	);
 };
-const AgendaOpenSlotCard = (): JSX.Element => (
-	<VStack
-		p="8px"
-		borderRadius="8px"
-		w="100%"
-		h="100%"
-		bg="blue.50"
-		border="2px solid"
-		borderColor="blue.200"
-		align="start"
-	>
-		<Box w="3px" minH="100%" bg="green.500" borderRadius="8px" />
-	</VStack>
-);
+const AgendaOpenSlotCard = ({ slotId }: { slotId: string }): JSX.Element => {
+	const [triggerCloseSlot] = useCloseSlotMutation();
+
+	const { isOpen: isHover, onOpen: onHoverOpen, onClose: onHoverClose } = useDisclosure();
+
+	const toast = useToast({ duration: 3000, isClosable: true });
+
+	return (
+		<VStack
+			p="8px"
+			borderRadius="8px"
+			w="100%"
+			h="100%"
+			bg={isHover ? 'grey.50' : 'blue.50'}
+			border="2px solid"
+			borderColor={isHover ? 'grey.200' : 'blue.200'}
+			align={isHover ? 'center' : 'start'}
+			onMouseEnter={onHoverOpen}
+			onMouseLeave={onHoverClose}
+			cursor="pointer"
+			onClick={() => {
+				triggerCloseSlot(slotId)
+					.unwrap()
+					.then(() => {
+						toast({ title: 'Le créneaux a bien été fermé', status: 'success' });
+					})
+					.catch(() => {
+						toast({ title: 'Une erreur est survenue', status: 'error' });
+					});
+			}}
+		>
+			{isHover ? (
+				<HStack h="100%">
+					<Text size="sm">Fermer le créneau</Text>
+					<Icon as={CircleCrossIcon} w="16px" h="16px" color="grey.700" />
+				</HStack>
+			) : (
+				<Box w="3px" minH="100%" bg="green.500" borderRadius="8px" />
+			)}
+		</VStack>
+	);
+};
 const AgendaBookedSlotCard = ({ patientName }: { patientName: string }): JSX.Element => (
 	<HStack p="8px" borderRadius="8px" w="100%" h="100%" bg="blue.200">
 		<Box w="3px" h="100%" bg="red.500" borderRadius="8px" />
@@ -72,14 +101,16 @@ const AgendaSlotCard = ({
 	patientName,
 	startDate,
 	endDate,
+	slotId,
 }: {
 	type: AgendaSlotStatusType;
 	patientName?: string;
 	startDate: number;
 	endDate: number;
+	slotId: string;
 }): JSX.Element => {
 	if (type === 'BOOKED' && patientName) return <AgendaBookedSlotCard patientName={patientName} />;
-	if (type === 'OPEN') return <AgendaOpenSlotCard />;
+	if (type === 'OPEN') return <AgendaOpenSlotCard slotId={slotId} />;
 	return <AgendaClosedSlotCard startDate={startDate} endDate={endDate} />;
 };
 
