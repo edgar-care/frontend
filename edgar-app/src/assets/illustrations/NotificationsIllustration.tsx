fbe803065b80e417e747a76c90a648e16bd46fd4
Lucas LOUIS <lucas.louis@epitech.eu>
2024-01-17 18:13:58 +0100

    fix(notifications): update illustration of the modal

---
+++ edgar-app/src/assets/illustrations/NotificationsIllustration.tsx
@@ -0,0 +1,18 @@
+import { createIcon } from '@chakra-ui/react';
+
+const NotificationsIllustration = createIcon({
+	displayName: 'NotificationsIllustration',
+	viewBox: '0 0 48 48',
+	path: (
+		<g>
+			<rect width="48" height="48" rx="24" fill="#C3EAAC" />
+			<path d="M24 32C25.1046 32 26 31.1046 26 30H22C22 31.1046 22.8954 32 24 32Z" fill="#356B20" />
+			<path
+				d="M24.9952 17.099C24.9984 17.0665 25 17.0334 25 17C25 16.4477 24.5523 16 24 16C23.4477 16 23 16.4477 23 17C23 17.0334 23.0016 17.0665 23.0048 17.099C20.7202 17.5604 19 19.5793 19 22C19 23.0976 18.5 28 17 29H31C29.5 28 29 23.0976 29 22C29 19.5793 27.2798 17.5604 24.9952 17.099Z"
+				fill="#356B20"
+			/>
+		</g>
+	),
+});
+
+export default NotificationsIllustration;
