07a5d5cbdbc58df54863c28a4f45aaf8f42c9d49
Lucas LOUIS <lucas.louis@epitech.eu>
2024-01-26 16:38:11 +0100

    feat(dependencies): update dependencies and use of nodejs20

--- edgar-doctor/src/app/layout.tsx
+++ edgar-doctor/src/app/layout.tsx
@@ -1,18 +1,26 @@
-import { Metadata } from 'next';
+import { Suspense } from 'react';
+import { Metadata, Viewport } from 'next';
 
 import Providers from 'app/Providers';
 
 export const metadata: Metadata = {
 	title: 'edgar',
 	description: "Gagne du temps avec l'assistant virtuel du pré-diagnostic",
-	viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
 	icons: '/assets/logo/edgar-logo-tabs.svg',
+};
+
+export const viewport: Viewport = {
+	width: 'device-width',
+	initialScale: 1,
+	maximumScale: 1,
 };
 
 const RootLayout = ({ children }: { children: JSX.Element }): JSX.Element => (
 	<html lang="fr">
 		<body>
-			<Providers>{children}</Providers>
+			<Suspense>
+				<Providers>{children}</Providers>
+			</Suspense>
 		</body>
 	</html>
 );
