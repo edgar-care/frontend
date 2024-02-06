e064f56c9091754318f455240db672bcbd42dc13
Lucas LOUIS <lucas.louis@epitech.eu>
2024-02-06 18:49:06 +0100

    fix(landing): update cypress tests with typo fix

--- landing-pages/cypress/e2e/homePage.cy.ts
+++ landing-pages/cypress/e2e/homePage.cy.ts
@@ -26,24 +26,39 @@ describe('Good content on elements - Desktop', () => {
 		cy.get('#edgar-homePage-product-button').should('contain.text', 'En apprendre plus sur edgar');
 	});
 
-	it('Good content for texts', () => {
+	it('Good content for catch phrase', () => {
 		cy.get('#edgar-homePage-catchPhrase-text').should(
 			'contain.text',
 			"Gagne du temps avec l'assistant virtuel du pré-diagnostic.",
 		);
+	});
+
+	it('Good content for subtitle', () => {
 		cy.get('#edgar-homePage-subTitle1-text').should(
 			'contain.text',
 			"Marre d'attendre pour avoir un rendez-vous chez le médecin généraliste ?",
 		);
+	});
+
+	it('Good content for subtitle 2', () => {
 		cy.get('#edgar-homePage-subTitle2-text').should(
 			'contain.text',
-			"Un Français attend en moyenne 6 jours avant d'avoir un rendez-vous chez un médecin généraliste.",
+			"Un Français attend en moyenne 6 jours avant d'assister à son rendez-vous chez un médecin généraliste.",
 		);
+	});
+
+	it('Good content for product title', () => {
 		cy.get('#edgar-homePage-productTitle-text').should('contain.text', 'Pourquoi edgar ?');
+	});
+
+	it('Good content for product description', () => {
 		cy.get('#edgar-homePage-productDescription-text').should(
 			'contain.text',
-			'Malgré les améliorations en termes de prise de rendez-vous chez les médecins généralistes, en France, ces quelques dernières année, un français attend en moyenne 6 jours avant d’aller à son rendez-vous médical.Chez edgar, nous voulons réduire cette durée en diminuant le nombre de rendez-vous inutiles. Ces rendez-vous font perdre du temps aux patients, mais aussi aux médecins, qui nous le savons sont surchargés.',
+			'Malgré les améliorations en termes de prise de rendez-vous chez les médecins généralistes, en France, ces quelques dernières années, un Français attend en moyenne 6 jours avant d’aller à son rendez-vous médical.',
 		);
+	});
+
+	it('Good content for QA title', () => {
 		cy.get('#edgar-homePage-QATitle-text').should('contain.text', 'QUESTIONS FRÉQUEMMENT POSÉES');
 	});
 });
@@ -81,7 +96,7 @@ describe('Good redirection on elements - Desktop', () => {
 		cy.get('#edgar-homePage-product-button')
 			.click()
 			.url()
-			.should('eq', `${Cypress.env('url')}/product`);
+			.should('eq', `${Cypress.env('url')}/project`);
 	});
 });
 
@@ -113,24 +128,39 @@ describe('Good content on elements - Mobile', () => {
 		cy.get('#edgar-homePage-product-button').should('contain.text', 'En apprendre plus sur edgar');
 	});
 
-	it('Good content for texts', () => {
+	it('Good content for catch phrase', () => {
 		cy.get('#edgar-homePage-catchPhrase-text').should(
 			'contain.text',
 			"Gagne du temps avec l'assistant virtuel du pré-diagnostic.",
 		);
+	});
+
+	it('Good content for subtitle', () => {
 		cy.get('#edgar-homePage-subTitle1-text').should(
 			'contain.text',
 			"Marre d'attendre pour avoir un rendez-vous chez le médecin généraliste ?",
 		);
+	});
+
+	it('Good content for subtitle 2', () => {
 		cy.get('#edgar-homePage-subTitle2-text').should(
 			'contain.text',
-			"Un Français attend en moyenne 6 jours avant d'avoir un rendez-vous chez un médecin généraliste.",
+			"Un Français attend en moyenne 6 jours avant d'assister à son rendez-vous chez un médecin généraliste.",
 		);
+	});
+
+	it('Good content for product title', () => {
 		cy.get('#edgar-homePage-productTitle-text').should('contain.text', 'Pourquoi edgar ?');
+	});
+
+	it('Good content for product description', () => {
 		cy.get('#edgar-homePage-productDescription-text').should(
 			'contain.text',
-			'Malgré les améliorations en termes de prise de rendez-vous chez les médecins généralistes, en France, ces quelques dernières année, un français attend en moyenne 6 jours avant d’aller à son rendez-vous médical.Chez edgar, nous voulons réduire cette durée en diminuant le nombre de rendez-vous inutiles. Ces rendez-vous font perdre du temps aux patients, mais aussi aux médecins, qui nous le savons sont surchargés.',
+			'Malgré les améliorations en termes de prise de rendez-vous chez les médecins généralistes, en France, ces quelques dernières années, un Français attend en moyenne 6 jours avant d’aller à son rendez-vous médical.',
 		);
+	});
+
+	it('Good content for QA title', () => {
 		cy.get('#edgar-homePage-QATitle-text').should('contain.text', 'QUESTIONS FRÉQUEMMENT POSÉES');
 	});
 });
@@ -168,6 +198,6 @@ describe('Good redirection on elements - Mobile', () => {
 		cy.get('#edgar-homePage-product-button')
 			.click()
 			.url()
-			.should('eq', `${Cypress.env('url')}/product`);
+			.should('eq', `${Cypress.env('url')}/project`);
 	});
 });
