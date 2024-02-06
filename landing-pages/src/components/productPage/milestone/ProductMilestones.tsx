a019f762be95b8a22f9b7fef1936dd68424d2f52
Lucas LOUIS <lucas.louis@epitech.eu>
2024-02-06 17:45:51 +0100

    fix(landing-project): fix milestones content and display

--- landing-pages/src/components/productPage/milestone/ProductMilestones.tsx
+++ landing-pages/src/components/productPage/milestone/ProductMilestones.tsx
@@ -1,14 +1,14 @@
-import { Box, VStack } from '@chakra-ui/react';
+import { Box, Text, VStack } from '@chakra-ui/react';
 
-import MilestoneRoadmap from './MilestoneRoadmap';
 import MilestoneDetails from './MilestoneDetails';
 import ProductNewsletter from '../ProductNewsletter';
 
 const ProductMilestones = (): JSX.Element => (
-	<VStack p={{ base: '32px 32px', md: '32px 64px', xl: '96px 128px' }} spacing="96px" w="100%">
-		<VStack spacing="64px" w="100%">
-			<MilestoneRoadmap />
-			<Box w="100%" h="1px" bg="blue.400" />
+	<VStack p={{ base: '32px 32px', md: '32px 64px', xl: '96px 128px' }} spacing="64px" w="100%">
+		<VStack spacing="32px" w="100%" align="start" id="milestones">
+			<Text size={{ base: '3xl', xl: '6xl' }} id="edgar-productPage-milestonesTitle-text">
+				Les milestones d'edgar
+			</Text>
 			<MilestoneDetails />
 		</VStack>
 		<Box w="100%" h="1px" bg="blue.400" />
