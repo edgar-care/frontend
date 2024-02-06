f1b70182a503b8b51aedb4eecd340525cee74eee
Lucas LOUIS <lucas.louis@epitech.eu>
2024-02-06 18:48:38 +0100

    feat(landing): add loader when loading the page

---
+++ landing-pages/src/components/loader/LoadingScreen.tsx
@@ -0,0 +1,16 @@
+import { Center, Img, VStack } from '@chakra-ui/react';
+
+import GridLoader from 'components/loader/GridLoader';
+
+import colors from 'theme/foundations/colors';
+
+const LoadingScreen = (): JSX.Element => (
+	<Center w="100%" h="100vh" bg="blue.100">
+		<VStack p={{ base: '32px', sm: '64px' }} w="100%" spacing="64px">
+			<Img src="/assets/logo/colored-edgar-logo.svg" w="400px" />
+			<GridLoader size="64" speed="1.5" color={colors.blue[900]} />
+		</VStack>
+	</Center>
+);
+
+export default LoadingScreen;
