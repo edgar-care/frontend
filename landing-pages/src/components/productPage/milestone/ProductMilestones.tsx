788849476ffefc0f21225af431f725061470b215
Lucas LOUIS <lucas.louis@epitech.eu>
2024-02-22 14:13:14 +0100

    fix(landing-product): center the label of the milestones

--- landing-pages/src/components/productPage/milestone/ProductMilestones.tsx
+++ landing-pages/src/components/productPage/milestone/ProductMilestones.tsx
@@ -5,7 +5,7 @@ import ProductNewsletter from '../ProductNewsletter';
 
 const ProductMilestones = (): JSX.Element => (
 	<VStack p={{ base: '32px 32px', md: '32px 64px', xl: '96px 128px' }} spacing="64px" w="100%">
-		<VStack spacing="32px" w="100%" align="start" id="milestones">
+		<VStack spacing="32px" w="100%" id="milestones">
 			<Text size={{ base: '3xl', xl: '6xl' }} id="edgar-productPage-milestonesTitle-text">
 				Les milestones d'edgar
 			</Text>
