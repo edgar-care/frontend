aa0738baacc688091f1f3c6eae5f4a344c64af9e
Lucas LOUIS <lucas.louis@epitech.eu>
2024-02-22 19:36:46 +0100

    feat(patients): add display of the patient list

--- edgar-doctor/src/app/dashboard/patients/content.tsx
+++ edgar-doctor/src/app/dashboard/patients/content.tsx
@@ -1,54 +1,20 @@
 'use client';
 
-import { useState } from 'react';
 import { VStack, HStack, Button } from '@chakra-ui/react';
-// import { useDisclosure } from '@chakra-ui/react';
-// import Link from 'next/link';
 
-import PatientCard from 'components/app/dashboardPages/patients/PatientCard';
 import DashboardPageBanner from 'components/app/dashboardPages/DashboardPageBanner';
+import Patients from 'components/app/dashboardPages/patients/Patients';
 
-import { PatientType } from 'types/dashboard/patients/PatientType';
-
-const PatientsPageContent = (): JSX.Element => {
-	const [patientList] = useState<PatientType[]>([
-		{
-			id: '1',
-			date: Date.now(),
-			name: 'Edgar Assistant',
-			doctor: 'Vous',
-		},
-		{
-			id: '2',
-			date: Date.now(),
-			name: 'Edgar Assistant 2',
-			doctor: 'Docteur XX',
-		},
-	]);
-	// const { isOpen: isOpenAddModal, onOpen: onOpenAddModal, onClose: onCloseAddModal } = useDisclosure();
-
-	return (
-		<VStack w="100%" spacing="32px">
-			<DashboardPageBanner title="Mes patients" subTitle="Retrouvez toutes les informations de vos patients." />
-			<VStack w="100%" spacing="16px">
-				<HStack w="100%" spacing="16px">
-					<Button
-						size="md"
-						variant="primary"
-						whiteSpace="nowrap"
-						id="edgar-dashboardPatientsPage-addPatient-button"
-					>
-						Ajouter un patient
-					</Button>
-				</HStack>
-				<VStack w="100%" spacing="8px" align="start">
-					{patientList.map((patient) => (
-						<PatientCard key={patient.id} patient={patient} />
-					))}
-				</VStack>
-			</VStack>
+const PatientsPageContent = (): JSX.Element => (
+	<VStack w="100%" spacing="32px" h="100%">
+		<DashboardPageBanner title="Mes patients" subTitle="Retrouvez toutes les informations de vos patients." />
+		<VStack w="100%" spacing="16px" h="100%">
+			<HStack w="100%" spacing="16px">
+				<Button id="edgar-dashboardPatientsPage-addPatient-button">Ajouter un patient</Button>
+			</HStack>
+			<Patients />
 		</VStack>
-	);
-};
+	</VStack>
+);
 
 export default PatientsPageContent;
