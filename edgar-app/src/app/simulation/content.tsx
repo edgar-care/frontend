f8798b9fb182fff391e5c819c4e75f00f08b62ee
Jules <jules.magyari@epitech.eu>
2024-03-24 17:26:43 +0100

    fix(simulation): resolve the responsive

--- edgar-app/src/app/simulation/content.tsx
+++ edgar-app/src/app/simulation/content.tsx
@@ -1,27 +1,21 @@
 'use client';
 
-import { Text, HStack, Icon, Box } from '@chakra-ui/react';
+import { Text, Box } from '@chakra-ui/react';
 import Link from 'next/link';
 
 import SimulationLayout from 'components/simulationPages/SimulationLayout';
-
-import RightCircleArrowIcon from 'assets/icons/Arrow/RightCircleArrowIcon';
+import SimulationButton from 'components/simulationPages/SimulationButton';
 
 const SimulationContent = (): JSX.Element => (
 	<SimulationLayout>
 		<>
-			<Text size="3xl" color="white" maxW="1000px">
-				Bienvenue dans votre simulation, je m’appel Edgar et je serai votre assistant tout au long de cette
+			<Text size={{ base: '2xl', md: '3xl' }} color="white" maxW="1000px">
+				Bienvenue dans votre simulation, je m’appelle Edgar et je serai votre assistant tout au long de cette
 				simulation.
 			</Text>
-			<Box w="100%">
+			<Box w="100%" textAlign="end">
 				<Link href="/simulation/conditions">
-					<HStack w="100%" justify="end">
-						<Text size="boldXl" color="white">
-							Continuer
-						</Text>
-						<Icon as={RightCircleArrowIcon} w="24px" h="24px" color="white" />
-					</HStack>
+					<SimulationButton text="Continuer" />
 				</Link>
 			</Box>
 		</>
