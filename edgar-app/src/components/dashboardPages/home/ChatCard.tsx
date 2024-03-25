87bfdc5270717c532867f19b475b2914777c291b
Jules <jules.magyari@epitech.eu>
2024-03-25 01:20:36 +0100

    fix(dashboard-home-chat): Some style fix + add a MessageType

--- edgar-app/src/components/dashboardPages/home/ChatCard.tsx
+++ edgar-app/src/components/dashboardPages/home/ChatCard.tsx
@@ -1,30 +1,36 @@
-import { Box, HStack, Icon, Text, VStack } from '@chakra-ui/react';
-import { useState } from 'react';
+import { Box, HStack, Icon, Text, VStack, useDisclosure } from '@chakra-ui/react';
 
 import CircleRightArrowIcon from 'assets/icons/Arrow/Circle/CircleRightArrowIcon';
 
-interface Message {
-	id: number;
-	sender: string;
-	notifications: number;
-	date: Date;
-	lastMessage: string;
-}
+import CircleZeroIcon from 'assets/icons/CircleNumber/CircleZeroIcon';
+import CircleOneIcon from 'assets/icons/CircleNumber/CircleOneIcon';
+import CircleTwoIcon from 'assets/icons/CircleNumber/CircleTwoIcon';
+import CircleThreeIcon from 'assets/icons/CircleNumber/CircleThreeIcon';
+import CircleFourIcon from 'assets/icons/CircleNumber/CircleFourIcon';
+import CircleFiveIcon from 'assets/icons/CircleNumber/CircleFiveIcon';
+import CircleSixIcon from 'assets/icons/CircleNumber/CircleSixIcon';
+import CircleSevenIcon from 'assets/icons/CircleNumber/CircleSevenIcon';
+import CircleEightIcon from 'assets/icons/CircleNumber/CircleEightIcon';
+import CircleNineIcon from 'assets/icons/CircleNumber/CircleNineIcon';
+import CirclePlusIcon from 'assets/icons/CircleNumber/CirclePlusIcon';
 
-const formatMessageDate = (dateString: Date) => {
-	const date = new Date(dateString);
+import { MessageType } from 'types/dashboard/home/MessageType';
 
-	const day = date.getDate();
-	const month = ['janv', 'févr', 'mars', 'avr', 'mai', 'juin', 'juil', 'août', 'sept', 'oct', 'nov', 'déc'][
-		date.getMonth()
+const ChatCard = ({ message }: { message: MessageType }): JSX.Element => {
+	const { isOpen: isHover, onOpen: onHoverOpen, onClose: onHoverClose } = useDisclosure();
+	const icons = [
+		{ icon: CircleZeroIcon },
+		{ icon: CircleOneIcon },
+		{ icon: CircleTwoIcon },
+		{ icon: CircleThreeIcon },
+		{ icon: CircleFourIcon },
+		{ icon: CircleFiveIcon },
+		{ icon: CircleSixIcon },
+		{ icon: CircleSevenIcon },
+		{ icon: CircleEightIcon },
+		{ icon: CircleNineIcon },
 	];
 
-	return `${day} ${month}`;
-};
-
-const ChatCard = ({ message }: { message: Message }): JSX.Element => {
-	const formattedDate = formatMessageDate(message.date);
-	const [showHStack, setShowHStack] = useState(false);
 	return (
 		<HStack
 			bg="blue.100"
@@ -33,8 +39,8 @@ const ChatCard = ({ message }: { message: Message }): JSX.Element => {
 			border="2px"
 			borderColor="blue.200"
 			spacing="8px"
-			onMouseEnter={() => setShowHStack(true)}
-			onMouseLeave={() => setShowHStack(false)}
+			onMouseEnter={onHoverOpen}
+			onMouseLeave={onHoverClose}
 		>
 			<HStack w="100%" bg="white" borderRadius="16px" p="8px 16px" justify="space-between">
 				<HStack spacing="8px">
@@ -42,41 +48,40 @@ const ChatCard = ({ message }: { message: Message }): JSX.Element => {
 					<VStack align="start" spacing="0px">
 						<HStack>
 							<Text size="boldMd">{message.sender}</Text>
-							<Text fontStyle="italic" fontSize="md" color="grey.500">
+							<Text fontStyle="italic" fontSize="12" color="grey.500">
 								{/* Insérer - Médecin généraliste */}
 							</Text>
 						</HStack>
-						<Text fontStyle="italic" fontSize="md" color="grey.500">
+						<Text fontStyle="italic" fontSize="12" color="grey.500">
 							{message.lastMessage}
 						</Text>
 					</VStack>
 				</HStack>
 				<VStack spacing="4px" align="end">
-					<Text fontStyle="italic" fontSize="md" color="grey.500">
-						{formattedDate}
+					<Text fontStyle="italic" fontSize="12" color="grey.500">
+						{new Date(message.date).toLocaleDateString('fr-FR', {
+							month: 'short',
+							day: 'numeric',
+						})}
 					</Text>
 					<Box
 						borderRadius="50%"
 						w="16px"
 						h="16px"
-						bg="blue.700"
+						bg="white"
 						display="flex"
 						justifyContent="center"
 						alignItems="center"
 					>
 						{message.notifications > 9 ? (
-							<Text fontSize="12" color="white">
-								+
-							</Text>
+							<Icon as={CirclePlusIcon} w="16px" h="16px" color="blue.700" />
 						) : (
-							<Text fontSize="12" color="white">
-								{message.notifications}
-							</Text>
+							<Icon as={icons[message.notifications].icon} w="16px" h="16px" color="blue.700" />
 						)}
 					</Box>
 				</VStack>
 			</HStack>
-			{showHStack && (
+			{isHover && (
 				<HStack justifyContent="flex-start">
 					<Icon as={CircleRightArrowIcon} w="20px" h="20px" color="blue.700" m="0px 10px 0px 0px" />
 				</HStack>
