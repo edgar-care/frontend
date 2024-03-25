e9f477ca8af7a40dc35bf15856a79e203266a740
Jules <jules.magyari@epitech.eu>
2024-03-25 01:19:45 +0100

    feat(dashboard-home-chat): add new notifications icons

---
+++ edgar-app/src/assets/icons/CircleNumber/CirclePlusIcon.tsx
@@ -0,0 +1,18 @@
+import { createIcon } from '@chakra-ui/react';
+
+const CirclePlusIcon = createIcon({
+	displayName: 'CirclePlusIcon',
+	viewBox: '0 0 16 16',
+	path: (
+		<g fill="currentColor">
+			<path
+				fill-rule="evenodd"
+				clip-rule="evenodd"
+				d="M8 16C12.4183 16 16 12.4183 16 8C16 3.58172 12.4183 0 8 0C3.58172 0 0 3.58172 0 8C0 12.4183 3.58172 16 8 16ZM8.74 8.618H11.568V7.274H8.74V4.376H7.256V7.274H4.428V8.618H7.256V11.516H8.74V8.618Z"
+				fill="currentColor"
+			/>
+		</g>
+	),
+});
+
+export default CirclePlusIcon;
