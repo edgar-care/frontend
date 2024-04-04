dbc8127c74169edc44fc5d2cee27a3670381be06
Jules <jules.magyari@epitech.eu>
2024-04-04 17:42:18 +0200

    fix(appointments): little fix on pagination

--- edgar-doctor/src/utils/navigation/countMaxNumberPage.ts
+++ edgar-doctor/src/utils/navigation/countMaxNumberPage.ts
@@ -1,4 +1,4 @@
 const countMaxNumberPage = (data: unknown[], numberElementsPerPage: number): number =>
-    Math.ceil(data.length / numberElementsPerPage) || 1;
+	Math.ceil(data.length / numberElementsPerPage) || 1;
 
 export default countMaxNumberPage;
