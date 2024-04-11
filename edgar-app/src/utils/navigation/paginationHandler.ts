e7bcdef1ce09626089aa5e8979ef048b2468c53f
Jules <jules.magyari@epitech.eu>
2024-04-11 16:41:59 +0200

    fix(appointments): prettier fix

--- edgar-app/src/utils/navigation/paginationHandler.ts
+++ edgar-app/src/utils/navigation/paginationHandler.ts
@@ -1,4 +1,4 @@
 const paginationHandler = <Type>(data: Type[], pageIndex: number, numberOfElementsPerPage: number): Type[] =>
-    data.slice((pageIndex - 1) * numberOfElementsPerPage, pageIndex * numberOfElementsPerPage);
+	data.slice((pageIndex - 1) * numberOfElementsPerPage, pageIndex * numberOfElementsPerPage);
 
 export default paginationHandler;
