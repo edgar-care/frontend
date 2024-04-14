14af24630235ebe3a2d0fa35d1bf94b0743f3015
Lucas LOUIS <lucas.louis@epitech.eu>
2024-04-14 11:07:21 +0200

    feat(onboarding): add pagination for primary doctor selection

--- edgar-app/src/components/onboardingPages/personal/primaryDoctor/SelectPrimaryDoctorContent.tsx
+++ edgar-app/src/components/onboardingPages/personal/primaryDoctor/SelectPrimaryDoctorContent.tsx
@@ -1,9 +1,13 @@
 import { type Dispatch, type SetStateAction, useState } from 'react';
-import { Icon, Input, InputGroup, InputRightElement, VStack } from '@chakra-ui/react';
+import { Icon, Input, InputGroup, InputRightElement, Text, VStack } from '@chakra-ui/react';
 
 import SelectPrimaryDoctorCard from 'components/onboardingPages/personal/primaryDoctor/SelectPrimaryDoctorCard';
+import Pagination from 'components/navigation/Pagination';
 
 import { useGetDoctorsQuery } from 'services/request/doctor';
+
+import paginationHandler from 'utils/navigation/paginationHandler';
+import countMaxNumberPage from 'utils/navigation/countMaxNumberPage';
 
 import SearchIcon from 'assets/icons/SearchIcon';
 
@@ -17,7 +21,14 @@ const SelectPrimaryDoctorContent = ({
 	setPrimaryDoctorName: Dispatch<SetStateAction<string>>;
 }): JSX.Element => {
 	const { data: doctors } = useGetDoctorsQuery();
+	const [pageIndex, setPageIndex] = useState(1);
 	const [searchValue, setSearchValue] = useState('');
+
+	const filteredDoctors = doctors?.filter(
+		(doctor) =>
+			doctor.name.toLowerCase().includes(searchValue.toLowerCase()) ||
+			doctor.firstname.toLowerCase().includes(searchValue.toLowerCase()),
+	);
 
 	return (
 		<VStack w="100%" spacing="24px">
@@ -28,22 +39,26 @@ const SelectPrimaryDoctorContent = ({
 				</InputRightElement>
 			</InputGroup>
 			<VStack w="100%">
-				{doctors &&
-					doctors
-						.filter(
-							(doctor) =>
-								doctor.name.toLowerCase().includes(searchValue.toLowerCase()) ||
-								doctor.firstname.toLowerCase().includes(searchValue.toLowerCase()),
-						)
-						.map((doctor) => (
-							<SelectPrimaryDoctorCard
-								doctorInfos={doctor}
-								onChange={onChange}
-								onClose={onClose}
-								setPrimaryDoctorName={setPrimaryDoctorName}
-								key={doctor.id}
-							/>
-						))}
+				{filteredDoctors &&
+					paginationHandler(filteredDoctors, pageIndex, 5).map((doctor) => (
+						<SelectPrimaryDoctorCard
+							doctorInfos={doctor}
+							onChange={onChange}
+							onClose={onClose}
+							setPrimaryDoctorName={setPrimaryDoctorName}
+							key={doctor.id}
+						/>
+					))}
+				{filteredDoctors && filteredDoctors?.length === 0 && <Text>Aucun médecin trouvé</Text>}
+				{filteredDoctors && filteredDoctors?.length > 5 && (
+					<Pagination
+						pageIndex={pageIndex}
+						maxPageNumbers={countMaxNumberPage(filteredDoctors, 5)}
+						setPageIndex={setPageIndex}
+						variant="secondary"
+						size="small"
+					/>
+				)}
 			</VStack>
 			{/*	TODO: add pagination */}
 		</VStack>
