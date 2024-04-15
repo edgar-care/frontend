73370eda2154808a0143d4b0c6bc321183852fd8
Lucas LOUIS <lucas.louis@epitech.eu>
2024-04-15 10:49:04 +0200

    feat(simulation): update simulation button to handle color variant

--- edgar-app/src/components/simulationPages/SimulationButton.tsx
+++ edgar-app/src/components/simulationPages/SimulationButton.tsx
@@ -3,7 +3,13 @@ import { Text, HStack, Icon, Button, useDisclosure } from '@chakra-ui/react';
 import RightCircleArrowFillIcon from 'assets/icons/Arrow/Circle/RightCircleArrowFillIcon';
 import RightCircleArrowIcon from 'assets/icons/Arrow/Circle/RightCircleArrowIcon';
 
-const SimulationButton = ({ children }: { children: string }): JSX.Element => {
+const SimulationButton = ({
+	children,
+	variant = 'WHITE',
+}: {
+	children: string;
+	variant?: 'BLUE' | 'WHITE';
+}): JSX.Element => {
 	const { isOpen: isHover, onOpen: onHoverOpen, onClose: onHoverClose } = useDisclosure();
 
 	return (
@@ -11,21 +17,25 @@ const SimulationButton = ({ children }: { children: string }): JSX.Element => {
 			variant="fullGhost"
 			borderRadius="32px"
 			p="8px 16px"
-			_hover={{ bg: 'white' }}
+			_hover={{ bg: variant === 'WHITE' ? 'white' : 'blue.700' }}
 			onMouseEnter={onHoverOpen}
 			onMouseLeave={onHoverClose}
 			role="group"
 		>
 			<HStack w="100%" spacing="16px">
-				<Text size="boldXl" color="white" _groupHover={{ color: 'blue.700' }}>
+				<Text
+					size={{ base: 'boldLg', sm: 'boldXl' }}
+					color={variant === 'WHITE' ? 'white' : 'black'}
+					_groupHover={{ color: variant === 'WHITE' ? 'blue.700' : 'white' }}
+				>
 					{children}
 				</Text>
 				<Icon
 					as={isHover ? RightCircleArrowFillIcon : RightCircleArrowIcon}
 					w="24px"
 					h="24px"
-					color="white"
-					_groupHover={{ color: 'blue.700' }}
+					color={variant === 'WHITE' ? 'white' : 'black'}
+					_groupHover={{ color: variant === 'WHITE' ? 'blue.700' : 'white' }}
 				/>
 			</HStack>
 		</Button>
