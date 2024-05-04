40d3cea04afb4b8847c82cc7fd6573319d473928
Jules <jules.magyari@epitech.eu>
2024-05-04 13:56:19 +0200

    fix(appointments): add missing enum

--- edgar-doctor/src/types/app/dashboard/appointments/AppointmentStatusType.ts
+++ edgar-doctor/src/types/app/dashboard/appointments/AppointmentStatusType.ts
@@ -1,1 +1,6 @@
-export type AppointmentStatusType = 'OPENED' | 'WAITING_FOR_REVIEW' | 'CANCELED';
+export type AppointmentStatusType =
+	| 'OPENED'
+	| 'WAITING_FOR_REVIEW'
+	| 'CANCELED'
+	| 'ACCEPTED_DUE_TO_REVIEW'
+	| 'CANCELED_DUE_TO_REVIEW';
