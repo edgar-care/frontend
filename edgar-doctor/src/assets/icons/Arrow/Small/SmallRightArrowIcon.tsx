7edc8ce5d1a4b96555f4fb40a8bbc0ede734e1e5
Lucas LOUIS <lucas.louis@epitech.eu>
2024-05-21 01:12:40 +0200

    feat(assets): add arrow and circleNumber assets

---
+++ edgar-doctor/src/assets/icons/Arrow/Small/SmallRightArrowIcon.tsx
@@ -0,0 +1,18 @@
+import { createIcon } from '@chakra-ui/react';
+
+const SmallRightArrowIcon = createIcon({
+	displayName: 'SmallRightArrowIcon',
+	viewBox: '0 0 8 8',
+	path: (
+		<g fill="currentColor">
+			<path
+				fillRule="evenodd"
+				clipRule="evenodd"
+				d="M0 4C0 3.72386 0.223858 3.5 0.5 3.5H6.29289L4.14645 1.35355C3.95118 1.15829 3.95118 0.841708 4.14645 0.646446C4.34171 0.451184 4.65829 0.451184 4.85355 0.646446L7.85355 3.64645C8.04882 3.84171 8.04882 4.15829 7.85355 4.35355L4.85355 7.35355C4.65829 7.54882 4.34171 7.54882 4.14645 7.35355C3.95118 7.15829 3.95118 6.84171 4.14645 6.64645L6.29289 4.5H0.5C0.223858 4.5 0 4.27614 0 4Z"
+				fill="currentColor"
+			/>
+		</g>
+	),
+});
+
+export default SmallRightArrowIcon;
