de4cb0bee12fd76dcc4697b60e6885096f7b9040
Jules <jules.magyari@epitech.eu>
2024-07-04 14:22:30 +0200

    feat(treatment): add the new icon

---
+++ edgar-app/src/assets/icons/TreatmentCardIcon/Days/LIcon.tsx
@@ -0,0 +1,17 @@
+import { createIcon } from '@chakra-ui/react';
+
+const LIcon = createIcon({
+	displayName: 'LIcon',
+	viewBox: '0 0 20 20',
+	path: (
+		<g fill="none">
+			<rect width="20" height="20" rx="4" fill="currentColor" />
+			<path
+				d="M9.146 13.212H12.436V14.5H7.55V4.77H9.146V13.212Z"
+				fill="white"
+			/>
+		</g>
+	),
+});
+
+export default LIcon;
---
+++ edgar-app/src/assets/icons/TreatmentCardIcon/Days/MIcon.tsx
@@ -0,0 +1,17 @@
+import { createIcon } from '@chakra-ui/react';
+
+const MIcon = createIcon({
+	displayName: 'MIcon',
+	viewBox: '0 0 20 20',
+	path: (
+		<g fill="none">
+			<rect width="20" height="20" rx="4" fill="currentColor" />
+			<path
+				d="M15.131 4.77V14.5H13.535V7.836L10.567 14.5H9.46101L6.47901 7.836V14.5H4.88301V4.77H6.60501L10.021 12.4L13.423 4.77H15.131Z"
+				fill="white"
+			/>
+		</g>
+	),
+});
+
+export default MIcon;
