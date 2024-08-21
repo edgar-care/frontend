228e25f9b98943243cb9e5a10e7c8ed6b5c8a8a2
Lucas LOUIS <lucas.louis@epitech.eu>
2024-08-21 15:10:45 +0200

    feat(2fa): add pagination when enabling 2fa with edgar app

--- edgar-app/src/components/settingsModals/pages/account/SettingsAccount2FAEdgarEnablePage.tsx
+++ edgar-app/src/components/settingsModals/pages/account/SettingsAccount2FAEdgarEnablePage.tsx
@@ -1,13 +1,19 @@
 import { useState } from 'react';
 import { Box, Button, VStack } from '@chakra-ui/react';
 
+import DeviceCard from 'components/settingsModals/DeviceCard';
+import Pagination from 'components/navigation/Pagination';
+
 import type { SettingsPageType } from 'types/navigation/SettingsPageType';
 import type { DeviceType } from 'types/dashboard/devices/DeviceType';
 
 import ShieldIllustration from 'assets/illustrations/ShieldIllustration';
-import DeviceCard from 'components/settingsModals/DeviceCard';
+
+import countMaxNumberPage from 'utils/navigation/countMaxNumberPage';
+import paginationHandler from 'utils/navigation/paginationHandler';
 
 const SettingsAccount2FAEdgarEnablePage = (onPrevious: () => void, onNext: () => void): SettingsPageType => {
+	const [pageIndex, setPageIndex] = useState(1);
 	const [selectedDevice, setSelectedDevice] = useState('');
 
 	const devices: DeviceType[] = [
@@ -49,7 +55,7 @@ const SettingsAccount2FAEdgarEnablePage = (onPrevious: () => void, onNext: () =>
 		sections: [],
 		bodyContent: (
 			<VStack w="100%" p="8px" borderRadius="16px" border="2px solid" borderColor="blue.100">
-				{devices.map((device, index) => (
+				{paginationHandler(devices, pageIndex, 5).map((device, index) => (
 					<VStack spacing="8px" key={device.id} w="100%">
 						{index > 0 && <Box as="span" w="100%" h="2px" bg="blue.100" />}
 						<DeviceCard
@@ -60,6 +66,15 @@ const SettingsAccount2FAEdgarEnablePage = (onPrevious: () => void, onNext: () =>
 						/>
 					</VStack>
 				))}
+				{devices.length > 5 && (
+					<Pagination
+						variant="secondary"
+						size="small"
+						pageIndex={pageIndex}
+						maxPageNumbers={countMaxNumberPage(devices, 5)}
+						setPageIndex={setPageIndex}
+					/>
+				)}
 			</VStack>
 		),
 		footerPrimaryButton: (
