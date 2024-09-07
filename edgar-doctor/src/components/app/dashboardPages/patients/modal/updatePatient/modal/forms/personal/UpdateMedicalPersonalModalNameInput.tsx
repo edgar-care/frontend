a2047ab6c6df098277b322cc0ed710e7cc05684e
Lucas LOUIS <lucas.louis@epitech.eu>
2024-09-07 16:59:51 +0200

    feat(patients): add update patient medical info modal

---
+++ edgar-doctor/src/components/app/dashboardPages/patients/modal/updatePatient/modal/forms/personal/UpdateMedicalPersonalModalNameInput.tsx
@@ -0,0 +1,34 @@
+import { FormLabel, Input, VStack } from '@chakra-ui/react';
+import { type FieldErrors, type UseFormRegister } from 'react-hook-form';
+
+import ErrorMessage from 'components/forms/ErrorMessage';
+
+import type { PatientMedicalInfosType } from 'types/app/dashboard/patients/PatientType';
+
+const updateMedicalPersonalModalNameInput = ({
+	register,
+	errors,
+}: {
+	register: UseFormRegister<PatientMedicalInfosType>;
+	errors: FieldErrors<PatientMedicalInfosType>;
+}): JSX.Element => (
+	<VStack spacing="4px" align="start" w="100%">
+		<FormLabel size="lg" id="edgar-updateMedicalPersonalModal-formName-text">
+			Votre nom
+		</FormLabel>
+		<Input
+			{...register('name', { minLength: 1, maxLength: 50, required: true })}
+			placeholder="L’assistant numérique"
+			w="100%"
+			maxLength={50}
+			id="edgar-updateMedicalPersonalModal-formName-input"
+		/>
+		{errors.name?.type === 'required' && (
+			<ErrorMessage id="edgar-updateMedicalPersonalModal-formNameError-text">
+				Ce champ est nécessaire
+			</ErrorMessage>
+		)}
+	</VStack>
+);
+
+export default updateMedicalPersonalModalNameInput;
