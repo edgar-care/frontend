eac581521fd8cbe8a83692b89c6180e7e330f952
Lucas LOUIS <lucas.louis@epitech.eu>
2024-09-10 22:45:57 +0200

    feat(services): handle 409 return status from backend & redirect the user to the login page

--- edgar-app/src/services/apiService.ts
+++ edgar-app/src/services/apiService.ts
@@ -25,7 +25,7 @@ export const eventEmitter = new EventEmitter();
 
 const baseQuery: BaseQueryFn<string | FetchArgs, unknown, FetchBaseQueryError> = async (args, api, extraOptions) => {
 	const result = await apiBase(args, api, extraOptions);
-	if (result.error && result.error.status === 401) {
+	if (result.error && (result.error.status === 401 || result.error.status === 409)) {
 		eventEmitter.emit('logout');
 		localStorage.removeItem('token');
 	}
