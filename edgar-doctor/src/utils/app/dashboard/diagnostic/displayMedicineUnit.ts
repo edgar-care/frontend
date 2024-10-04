e5ab78d5a6cb3378ea7881f169bd373f34a4acd7
Lucas LOUIS <lucas.louis@epitech.eu>
2024-10-04 22:55:12 +0200

    feat(medicines): update util functions to display the medicine dosage form and usage form

--- edgar-doctor/src/utils/app/dashboard/diagnostic/displayMedicineUnit.ts
+++ edgar-doctor/src/utils/app/dashboard/diagnostic/displayMedicineUnit.ts
@@ -1,11 +1,25 @@
-import { type MedicineUnitType } from 'types/app/dashboard/diagnostics/MedicineType';
+import type { MedicineUnitType } from 'types/app/dashboard/diagnostics/MedicineType';
 
 const displayMedicineUnit = (unit: MedicineUnitType): string => {
 	const units: { [key: string]: string } = {
-		APPLICATION: 'application',
-		TABLET: 'comprimé',
-		TABLESPOON: 'cuillère à soupe',
-		COFFEESPOON: 'cuillère à café',
+		CREME: 'Crème',
+		POMMADE: 'Pommade',
+		GELULE: 'Gélule',
+		COMPRIME: 'Comprimé',
+		GELE: 'Gel',
+		SOLUTION_BUVABLE: 'Solution buvable',
+		POUDRE: 'Poudre',
+		SUPPOSITOIRE: 'Suppositoire',
+		AMPOULE: 'Ampoule',
+		SUSPENSION_NASALE: 'Suspension nasale',
+		SPRAY: 'Spray',
+		COLLUTOIRE: 'Collutoire',
+		SHAMPOOING: 'Shampooing',
+		SOLUTION_INJECTABLE: 'Solution injectable',
+		COMPRIMER_EFERVESCENT: 'Comprimé effervescent',
+		GRANULER_EN_SACHET: 'Granulé en sachet',
+		PASTILLE: 'Pastille',
+		SIROP: 'Sirop',
 	};
 
 	return units[unit];
---
+++ edgar-doctor/src/utils/app/dashboard/diagnostic/displayMedicineUsageUnit.ts
@@ -0,0 +1,28 @@
+import type { MedicineUnitType } from 'types/app/dashboard/diagnostics/MedicineType';
+
+const displayMedicineUsageUnit = (unit: MedicineUnitType): string => {
+	const units: { [key: string]: string } = {
+		CREME: 'application',
+		POMMADE: 'application',
+		GELULE: 'gélule',
+		COMPRIME: 'comprimé',
+		GELE: 'application',
+		SOLUTION_BUVABLE: 'dose',
+		POUDRE: 'sachet',
+		SUPPOSITOIRE: 'suppositoire',
+		AMPOULE: 'ampoule',
+		SUSPENSION_NASALE: 'utilisation nasale',
+		SPRAY: 'utilisation',
+		COLLUTOIRE: 'utilisation',
+		SHAMPOOING: 'application',
+		SOLUTION_INJECTABLE: 'solution injectable',
+		COMPRIMER_EFERVESCENT: 'comprimé effervescent',
+		GRANULER_EN_SACHET: 'sachet',
+		PASTILLE: 'pastille',
+		SIROP: 'dose',
+	};
+
+	return units[unit];
+};
+
+export default displayMedicineUsageUnit;
