c4de2c0c4e9fc350476ac543617b4f2da8d33f9a
LeBaku <jules.magyari@epitech.eu>
2024-10-12 15:00:03 +0200

    feat(register): create the request and the DTO

--- edgar-doctor/src/services/request/account.ts
+++ edgar-doctor/src/services/request/account.ts
@@ -1,6 +1,6 @@
 import { backendApi } from 'services/apiService';
 
-import { type MissingPasswordDTO, type ResetPasswordDTO } from 'store/types/account.type';
+import { type MissingPasswordDTO, type ResetPasswordDTO, type RegisterTypeDTO } from 'store/types/account.type';
 
 const extendedApi = backendApi.injectEndpoints({
 	endpoints: (builder) => ({
@@ -22,7 +22,25 @@ const extendedApi = backendApi.injectEndpoints({
 				},
 			}),
 		}),
+		register: builder.mutation<void, RegisterTypeDTO>({
+			query: (params) => ({
+				url: '/auth/d/register',
+				method: 'POST',
+				body: {
+					email: params.email,
+					password: params.password,
+					name: params.name,
+					firstname: params.firstname,
+					adress: {
+						street: params.address.street,
+						zip_code: params.address.zip_code,
+						country: params.address.country,
+						city: params.address.city,
+					},
+				},
+			}),
+		}),
 	}),
 });
 
-export const { useMissingPasswordMutation, useResetPasswordMutation } = extendedApi;
+export const { useMissingPasswordMutation, useResetPasswordMutation, useRegisterMutation } = extendedApi;
--- edgar-doctor/src/store/types/account.type.ts
+++ edgar-doctor/src/store/types/account.type.ts
@@ -6,3 +6,16 @@ export interface ResetPasswordDTO {
 	password: string;
 	uuid: string;
 }
+
+export interface RegisterTypeDTO {
+	email: string;
+	password: string;
+	name: string;
+	firstname: string;
+	address: {
+		street: string;
+		city: string;
+		zip_code: string;
+		country: string;
+	};
+}
