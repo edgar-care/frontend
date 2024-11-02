d04cb97cea25200c364eaada96350911c721de46
Lucas LOUIS <lucas.louis@epitech.eu>
2024-11-02 12:04:23 +0100

    feat(login-2fa): add login 2fa page and modals for each 2fa methods

---
+++ edgar-app/src/components/connectionPage/login-2fa/Login2faModalPages.tsx
@@ -0,0 +1,44 @@
+import { type Dispatch, type SetStateAction, useEffect } from 'react';
+
+import Login2faModalEmailPage from 'components/connectionPage/login-2fa/pages/Login2faModalEmailPage';
+import Login2faModalAuthenticatorPage from 'components/connectionPage/login-2fa/pages/Login2faModalAuthenticatorPage';
+import Login2faModalBackupCodePage from 'components/connectionPage/login-2fa/pages/Login2faModalBackupCodePage';
+import Login2faModalMobileStartPage from 'components/connectionPage/login-2fa/pages/Login2faModalMobileStartPage';
+import Login2faModalMobileWaitingPage from 'components/connectionPage/login-2fa/pages/Login2faModalMobileWaitingPage';
+import Login2faModalMobileAcceptedPage from 'components/connectionPage/login-2fa/pages/Login2faModalMobileAcceptedPage';
+import Login2faModalMobileRefusedPage from 'components/connectionPage/login-2fa/pages/Login2faModalMobileRefusedPage';
+
+import { useLogin2faMobileContext } from 'contexts/login2faMobile';
+
+import type { Login2faModalPageType } from 'types/login-2fa/Login2faModalPageType';
+import type { Login2faPageType } from 'types/login-2fa/Login2faPageType';
+
+const Login2faModalPages = ({
+	onClose,
+	selectedPage,
+	setSelectedPage,
+}: {
+	onClose: () => void;
+	selectedPage: Login2faModalPageType;
+	setSelectedPage: Dispatch<SetStateAction<Login2faModalPageType>>;
+}): Record<Login2faModalPageType, Login2faPageType> => {
+	const { code, response, actions } = useLogin2faMobileContext();
+
+	useEffect(() => {
+		if (selectedPage !== 'mobileWaiting') return;
+		if (response === true) setSelectedPage('mobileAccepted');
+		if (response === false) setSelectedPage('mobileRefused');
+	}, [response]);
+
+	return {
+		email: Login2faModalEmailPage({ onClose, selectedPage }),
+		authenticator: Login2faModalAuthenticatorPage({ onClose }),
+		mobileStart: Login2faModalMobileStartPage({ actions, selectedPage, setSelectedPage }),
+		mobileWaiting: Login2faModalMobileWaitingPage({ setSelectedPage }),
+		mobileAccepted: Login2faModalMobileAcceptedPage({ code, selectedPage, onClose }),
+		mobileRefused: Login2faModalMobileRefusedPage({ actions, setSelectedPage }),
+		backupCode: Login2faModalBackupCodePage({ onClose }),
+	};
+};
+
+export default Login2faModalPages;
