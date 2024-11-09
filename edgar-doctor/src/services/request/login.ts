24fa5511c81d972c4e78331507c0536f56fb1f37
Lucas LOUIS <lucas.louis@epitech.eu>
2024-11-09 10:42:36 +0100

    feat(login-2fa): add backend services

---
+++ edgar-doctor/src/services/request/login.ts
@@ -0,0 +1,77 @@
+import { backendApi } from 'services/apiService';
+
+import type { Login2faDTO } from 'store/types/login.type';
+
+const extendedApi = backendApi.injectEndpoints({
+	endpoints: (builder) => ({
+		sendLogin2faEmail: builder.mutation<void, string>({
+			query: (email) => ({
+				url: '/auth/d/sending_email',
+				method: 'POST',
+				body: {
+					email,
+				},
+			}),
+		}),
+
+		loginWithEmail: builder.mutation<string, Login2faDTO>({
+			query: (params) => ({
+				url: '/auth/d/email_2fa',
+				method: 'POST',
+				body: {
+					email: params.email,
+					password: params.password,
+					token_2fa: params.code,
+				},
+			}),
+			transformResponse: (response: { token: string }) => response.token,
+		}),
+
+		loginWithAuthenticator: builder.mutation<string, Login2faDTO>({
+			query: (params) => ({
+				url: '/auth/d/third_party_2fa',
+				method: 'POST',
+				body: {
+					email: params.email,
+					password: params.password,
+					token_2fa: params.code,
+				},
+			}),
+			transformResponse: (response: { token: string }) => response.token,
+		}),
+
+		loginWithMobile: builder.mutation<string, Login2faDTO>({
+			query: (params) => ({
+				url: '/auth/d/mobile_2fa',
+				method: 'POST',
+				body: {
+					email: params.email,
+					password: params.password,
+					token_2fa: params.code,
+				},
+			}),
+			transformResponse: (response: { token: string }) => response.token,
+		}),
+
+		loginWithBackupCodes: builder.mutation<string, Login2faDTO>({
+			query: (params) => ({
+				url: '/auth/d/backup_code_2fa',
+				method: 'POST',
+				body: {
+					email: params.email,
+					password: params.password,
+					backup_code: params.code,
+				},
+			}),
+			transformResponse: (response: { token: string }) => response.token,
+		}),
+	}),
+});
+
+export const {
+	useSendLogin2faEmailMutation,
+	useLoginWithEmailMutation,
+	useLoginWithAuthenticatorMutation,
+	useLoginWithMobileMutation,
+	useLoginWithBackupCodesMutation,
+} = extendedApi;
