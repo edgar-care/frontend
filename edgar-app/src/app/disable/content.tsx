f4c323dae540519a16cf8476f178e3db2d71524f
LeBaku <jules.magyari@epitech.eu>
2024-11-15 15:12:59 +0100

    feat(account-disable): add a page to enable the account

--- edgar-app/src/app/disable/content.tsx
+++ edgar-app/src/app/disable/content.tsx
@@ -1,38 +1,22 @@
 'use client';
 
-import { useEffect } from 'react';
-import { HStack, useBreakpointValue, VStack } from '@chakra-ui/react';
-import { useRouter, useSearchParams } from 'next/navigation';
+import { VStack, useBreakpointValue, HStack } from '@chakra-ui/react';
+
+import DisablePageFields from 'components/connectionPage/disable/DisablePageFields';
+import DisablePageBanner from 'components/connectionPage/disable/DisablePageBanner';
 
 const DisablePageContent = (): JSX.Element => {
-	const searchParams = useSearchParams();
-	const router = useRouter();
-
 	const isTablet = useBreakpointValue({ base: true, lg: false });
 
 	return (
-		<VStack
-			p={{ base: '0px', sm: '16px', lg: '64px', xl: '128px', '4xl': '128px 256px' }}
-			w="100%"
-			h={{ base: '100%', lg: 'auto' }}
-			justify={{ base: 'start', sm: 'center' }}
-			minH={{ base: 'auto', sm: '100vh' }}
-		>
-			{/* {isTablet ? (
-				<VStack
-					bg="white"
-					borderRadius={{ base: '0px', sm: '48px' }}
-					w="100%"
-					border={{ base: '0px', sm: '2px solid' }}
-					borderColor={{ sm: 'blue.200' }}
-					align="stretch"
-					spacing="16px"
-					h="100%"
-				>
-					<ConnectionEdgarCard />
-					<ConnectionLoginFields />
-				</VStack>
-			) : (
+		<>
+			<VStack
+				p={{ base: '0px', sm: '16px 128px', lg: '64px 256px', xl: '128px 512px' }}
+				w="100%"
+				h={{ base: '100%', lg: 'auto' }}
+				justify={{ base: 'start', sm: 'center' }}
+				minH={{ base: 'auto', sm: '100vh' }}
+			>
 				<HStack
 					bg="white"
 					borderRadius="48px"
@@ -40,14 +24,16 @@ const DisablePageContent = (): JSX.Element => {
 					border="2px solid"
 					borderColor="blue.200"
 					align="stretch"
-					spacing="0px"
-					maxW="1750px"
+					spacing="32px"
+					p="32px"
+					h="100%"
+					maxW="896px"
 				>
-					<ConnectionEdgarCard />
-					<ConnectionLoginFields />
+					{!isTablet && <DisablePageBanner />}
+					<DisablePageFields />
 				</HStack>
-			)} */}
-		</VStack>
+			</VStack>
+		</>
 	);
 };
 
