f023418de3f603ed09bc0cc089faa429f908d2d4
LeBaku <jules.magyari@epitech.eu>
2024-11-19 10:00:00 +0100

    fix(dashboard-delete-account): switch PUT to POST

--- edgar-doctor/src/services/request/auth.ts
+++ edgar-doctor/src/services/request/auth.ts
@@ -5,7 +5,7 @@ const extendedApi = backendApi.injectEndpoints({
 		deleteAccount: builder.mutation<void, void>({
 			query: () => ({
 				url: '/auth/delete_account',
-				method: 'PUT',
+				method: 'POST',
 			}),
 		}),
 	}),
