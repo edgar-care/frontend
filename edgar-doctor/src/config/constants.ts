48ee9040c267e7b3f862e193f1c0a7a7ad66154b
Lucas LOUIS <lucas.louis@epitech.eu>
2024-11-23 18:44:43 +0100

    feat(env): udpate env variables by adding a secret_encrypt_key

--- edgar-doctor/src/config/constants.ts
+++ edgar-doctor/src/config/constants.ts
@@ -1,6 +1,10 @@
 if (!process.env.NEXT_PUBLIC_API_URL) throw new Error('NEXT_PUBLIC_API_URL env variable must be set');
 if (!process.env.NEXT_PUBLIC_WS_URL) throw new Error('NEXT_PUBLIC_WS_URL env variable must be set');
 
-// eslint-disable-next-line import/prefer-default-export
+if (!process.env.NEXT_PUBLIC_SECRET_ENCRYPT_KEY)
+	throw new Error('NEXT_PUBLIC_SECRET_ENCRYPT_KEY env variable must be set');
+
 export const API_URL = process.env.NEXT_PUBLIC_API_URL;
 export const WS_URL = process.env.NEXT_PUBLIC_WS_URL;
+
+export const SECRET_ENCRYPT_KEY = process.env.NEXT_PUBLIC_SECRET_ENCRYPT_KEY;
